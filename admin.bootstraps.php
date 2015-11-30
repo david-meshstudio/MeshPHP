efaf67e3dce20b9d96e44668ac3fc41e42cd015e
david-meshstudio <david@mesh-studio.com>
2015-11-30 16:05:09 +0800

    corrected referance paths

--- admin.bootstraps.php
+++ admin.bootstraps.php
@@ -7,5 +7,6 @@ include_once('includes/menu.class.php');
 include_once('base/base.common.php');
 include_once('base/base.appmanage.php');
 // util
-include_once('/util/tools.php');
-include_once('/util/mdbadaptor.class.php');
+include_once('util/tools.php');
+include_once('util/mdbadaptor.class.php');
+include_once('util/mdbo.class.php');
